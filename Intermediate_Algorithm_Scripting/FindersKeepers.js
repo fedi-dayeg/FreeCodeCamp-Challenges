607828ba2e563b32ef2bc89abe8798e9fea3a6d2
Sam <sam@Sam-C-Allicks-MacBook-Air.local>
2016-03-29 18:50:45 -0400

    Push FindersKeepers.js to Intermediate_Algorithm_Scripting

---
+++ Intermediate_Algorithm_Scripting/FindersKeepers.js
@@ -0,0 +1,13 @@
+function find(arr, func) {
+var num = 0;
+var myArray = [];
+
+for (var i = 0; i < arr.length; i++) {
+  if (func(arr[i]) === true) {
+  myArray.push(arr[i]);
+}
+}
+return myArray[0];
+}
+
+find([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
