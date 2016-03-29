acc2b98650bec7978ec3755f6f75a858efab44f5
Sam <sam@Sam-C-Allicks-MacBook-Air.local>
2016-03-29 19:02:08 -0400

    Updated FindersKeepers Intermediate Algoritm Solution

--- Intermediate_Algorithm_Scripting/FindersKeepers.js
+++ Intermediate_Algorithm_Scripting/FindersKeepers.js
@@ -1,13 +1,29 @@
 function find(arr, func) {
-var num = 0;
 var myArray = [];
 
+// Loop through array.
 for (var i = 0; i < arr.length; i++) {
+  // If array element in function returns true,
   if (func(arr[i]) === true) {
-  myArray.push(arr[i]);
+    // Push array element to myArray.
+    myArray.push(arr[i]);
 }
 }
+// Return first element in myArray.
 return myArray[0];
 }
 
+// This is the input for our function.
 find([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
+
+/*
+Finders Keepers
+
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Array.filter()
+*/
