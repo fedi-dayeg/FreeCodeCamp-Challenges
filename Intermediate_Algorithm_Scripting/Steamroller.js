43d2c297f71b3e0548eac40ecd80581d9320ac05
Sam <sam@Sam-C-Allicks-MacBook-Air.local>
2016-03-29 22:13:19 -0400

    Add Steamroller.js to Intermediate_Algorithm_Scripting

---
+++ Intermediate_Algorithm_Scripting/Steamroller.js
@@ -0,0 +1,33 @@
+/*
+Steamroller
+Flatten a nested array. You must account for varying levels of nesting.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Array.isArray()
+*/
+
+function steamroller(arr) {
+var myArray = [];
+
+myArray = arr.reduce(function(a,b) {
+  if (Array.isArray(b)) {
+     return a.concat(steamroller(b));
+  } else {
+  return a.concat(b);
+}
+
+}, []);
+
+
+
+return myArray;
+
+
+
+
+}
+
+steamroller([1, [2], [3, [[4]]]]);
