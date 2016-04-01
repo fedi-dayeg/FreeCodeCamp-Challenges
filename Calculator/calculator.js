1a74a14b4b9face9db87bdc254ebc94598c19bad
Sam <sam-allick@hotmail.ca>
2016-04-01 14:39:05 -0400

    Init Calculator

---
+++ Calculator/calculator.js
@@ -0,0 +1,55 @@
+var currentCount = [];
+$( document ).ready(function() {
+
+  $("#oneButton").on("click", function() {
+    currentCount.push(1);
+    console.log(currentCount);
+});
+  $("#twoButton").on("click", function() {
+    currentCount.push(2);
+    console.log(currentCount);
+  });
+  $("#threeButton").on("click", function() {
+    currentCount.push(3);
+    console.log(currentCount);
+  });
+  $("#fourButton").on("click", function() {
+    currentCount.push(4);
+    console.log(currentCount);
+});
+  $("#fiveButton").on("click", function() {
+    currentCount.push(5);
+    console.log(currentCount);
+  });
+  $("#sixButton").on("click", function() {
+    currentCount.push(6);
+    console.log(currentCount);
+  });
+  $("#sevenButton").on("click", function() {
+    currentCount.push(7);
+    console.log(currentCount);
+  });
+  $("#eightButton").on("click", function() {
+    currentCount.push(8);
+    console.log(currentCount);
+  });
+  $("#nineButton").on("click", function() {
+    currentCount.push(9);
+    console.log(currentCount);
+  });
+  $("#zeroButton").on("click", function() {
+    currentCount.push(0);
+    console.log(currentCount);
+});
+
+});
+
+
+
+
+
+/*
+
+
+
+*/
