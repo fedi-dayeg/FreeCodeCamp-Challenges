361fbdf1729ec1870b7a02a9ef2a96297cd9c4f0
Sam <sam-allick@hotmail.ca>
2016-04-01 21:21:53 -0400

    Update Calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,55 +1,116 @@
+
 var currentCount = [];
+var numbersInCalculation = [];
+var myNumber;
+var output;
 $( document ).ready(function() {
+
+
+// The following process allows us to select multiple-digit-numbers
+// Display them, and save them for later use.
+// On click, push selected number to currentCount array.
+// Then save joined version of currentCount to myNumber.
+// But note that currentCount itself is not joined, remains intact.
+// Then set display text equal to myNumber.
 
   $("#oneButton").on("click", function() {
     currentCount.push(1);
-    console.log(currentCount);
-});
+    myNumber = currentCount.join('');
+    $("#display").text(myNumber);
+  });
   $("#twoButton").on("click", function() {
     currentCount.push(2);
-    console.log(currentCount);
+    myNumber = currentCount.join('');
+    $("#display").text(myNumber);
   });
   $("#threeButton").on("click", function() {
     currentCount.push(3);
-    console.log(currentCount);
+    myNumber = currentCount.join('');
+    $("#display").text(myNumber);
   });
   $("#fourButton").on("click", function() {
     currentCount.push(4);
-    console.log(currentCount);
-});
+    myNumber = currentCount.join('');
+    $("#display").text(myNumber);
+  });
   $("#fiveButton").on("click", function() {
     currentCount.push(5);
-    console.log(currentCount);
+    myNumber = currentCount.join('');
+    $("#display").text(myNumber);
   });
   $("#sixButton").on("click", function() {
     currentCount.push(6);
-    console.log(currentCount);
+    myNumber = currentCount.join('');
+    $("#display").text(myNumber);
   });
   $("#sevenButton").on("click", function() {
     currentCount.push(7);
-    console.log(currentCount);
+    myNumber = currentCount.join('');
+    $("#display").text(myNumber);
   });
   $("#eightButton").on("click", function() {
     currentCount.push(8);
-    console.log(currentCount);
+    myNumber = currentCount.join('');
+    $("#display").text(myNumber);
   });
   $("#nineButton").on("click", function() {
     currentCount.push(9);
-    console.log(currentCount);
+    myNumber = currentCount.join('');
+    $("#display").text(myNumber);
   });
   $("#zeroButton").on("click", function() {
     currentCount.push(0);
+    myNumber = currentCount.join('');
+    $("#display").text(myNumber);
+  });
+
+// OPERATORS
+
+  $("#addButton").on("click", function() {
+    numbersInCalculation.push(myNumber, "+");
+    currentCount = [];
     console.log(currentCount);
-});
+    console.log(numbersInCalculation);
+  });
+  $("#subtractButton").on("click", function() {
+    numbersInCalculation.push(myNumber, "-");
+    currentCount = [];
+    console.log(currentCount);
+    console.log(numbersInCalculation);
+  });
+  $("#multiplyButton").on("click", function() {
+    numbersInCalculation.push(myNumber, "*");
+    currentCount = [];
+    console.log(currentCount);
+    console.log(numbersInCalculation);
+  });
+  $("#divideButton").on("click", function() {
+    numbersInCalculation.push(myNumber, "/");
+    currentCount = [];
+    console.log(currentCount);
+    console.log(numbersInCalculation);
+  });
+  $("#equalButton").on("click", function() {
+    numbersInCalculation.push(myNumber);
+    output = numbersInCalculation.join(' ');
+    output = eval(output);
+    $("#display").text(output);
+    myNumber = null;
+    console.log(numbersInCalculation);
+  });
+  $("#cButton").on("click", function() {
+    numbersInCalculation = [];
+    currentCount = [];
+    myNumber = " ";
+    $("#display").text(myNumber);
+    console.log(numbersInCalculation);
+  });
 
-});
 
 
 
 
 
-/*
 
 
-
-*/
+}); // End document ready.
